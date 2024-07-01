8da3103e73ad32d1ad1c586dd02241a2ed29f050
ALi Rezk <133886728+arezk11@users.noreply.github.com>
2024-07-01 13:38:31 -0400

    Add files via upload

---
+++ Workshop1/P1/ShoppingList.h
@@ -0,0 +1,20 @@
+#ifndef SDDS_SHOPPINGLIST_H
+#define SDDS_SHOPPINGLIST_H
+
+#include "ShoppingRec.h"
+
+namespace sdds 
+{
+    bool loadList();
+    void displayList();
+    void removeBoughtItems();
+    void removeItem(int index);
+    bool saveList();
+    void clearList();
+    void toggleBought();
+    void addItemToList();
+    void removeItemfromList();
+    bool listIsEmpty();
+}
+
+#endif //SDDS_SHOPPINGLIST_H
